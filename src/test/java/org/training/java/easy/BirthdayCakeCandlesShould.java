2ad6694b992311af79d3bff6a69d61af020e7c7e
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-08-31 22:51:29 +0200

    Add birthday cake candles challenge unit tests class

---
+++ src/test/java/org/training/java/easy/BirthdayCakeCandlesShould.java
@@ -0,0 +1,12 @@
+package org.training.java.easy;
+
+import org.junit.jupiter.api.DisplayName;
+
+/**
+ * Unit tests for the birthday cake candles challenge.
+ * @author Solange U. Gasengayire
+ */
+@DisplayName("Birthday Cake Candles should")
+public class BirthdayCakeCandlesShould {
+
+}
