a06a927f22d75e01437c862088abf4ce6976c4eb
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-08-31 22:51:58 +0200

    Add climbing the leaderboard challenge unit tests class

---
+++ src/test/java/org/training/java/medium/ClimbingTheLeaderboardShould.java
@@ -0,0 +1,12 @@
+package org.training.java.medium;
+
+import org.junit.jupiter.api.DisplayName;
+
+/**
+ * Unit tests for the climbing the leaderboard challenge
+ * @author Solange U. Gasengayire
+ */
+@DisplayName("Climbing The Leaderboard should")
+public class ClimbingTheLeaderboardShould {
+
+}
