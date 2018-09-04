936b09368467d909afdb29fbc5abdadc5e089405
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-09-04 11:39:45 +0200

    Add test to make sure grades whose difference with the next five multiple is higher than 3 are not rounded

--- src/test/java/org/training/java/algorithms/easy/GradingStudentsTests.java
+++ src/test/java/org/training/java/algorithms/easy/GradingStudentsTests.java
@@ -42,4 +42,18 @@ class GradingStudentsTests {
                 () -> assertEquals(37, results[3])
         );
     }
+
+    @Test
+    @DisplayName("not round grades whose difference with the next five multiple is higher than 3")
+    void notRoundGradesWhoseDifferenceWithNextFiveMultipleIsHigherThanThree() {
+        int[] grades = {40, 51, 66, 87};
+        int[] results = GradingStudents.gradingStudents(grades);
+        assertAll("return unrounded grades for example case {40, 51, 66, 87}",
+                () -> assertEquals(4, results.length),
+                () -> assertEquals(40, results[0]),
+                () -> assertEquals(51, results[1]),
+                () -> assertEquals(66, results[2]),
+                () -> assertEquals(87, results[3])
+        );
+    }
 }
