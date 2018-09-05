d8e31c59296dfc6fe205227af8d73180a8a759a3
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-09-05 10:18:11 +0200

    Add unit tests to check correct minimal cost is returned for example cases

--- src/test/java/org/training/java/algorithms/medium/MagicSquareTests.java
+++ src/test/java/org/training/java/algorithms/medium/MagicSquareTests.java
@@ -45,5 +45,39 @@ class MagicSquareTests {
         }
     }
 
+    @Test
+    @DisplayName("should return 7, 1 and 4 as minimal costs for example cases")
+    void shouldReturnAppropriateMinimalCostForExampleCases() {
+        int[][] matrix = {{5, 3, 4},
+                          {1, 5, 8},
+                          {6, 4, 2}};
+        int cost = MagicSquare.formingMagicSquare(matrix);
+        assertEquals(7, cost);
+
+        matrix = new int[][]{
+                {4, 9, 2},
+                {3, 5, 7},
+                {8, 1, 5}};
+        cost = MagicSquare.formingMagicSquare(matrix);
+        assertEquals(1, cost);
+
+        matrix = new int[][]{
+                {4, 8, 2},
+                {4, 5, 7},
+                {6, 1, 6}};
+        cost = MagicSquare.formingMagicSquare(matrix);
+        assertEquals(4, cost);
+    }
+
+    @Test
+    @DisplayName("should return 0 as minimal cost for magic square input")
+    void shouldReturnZeroAsMinimalCostForMagicSquare() {
+        int[][] matrix = {{8, 3, 4},
+                {1, 5, 9},
+                {6, 7, 2}};
+        int cost = MagicSquare.formingMagicSquare(matrix);
+        assertEquals(0, cost);
+    }
+
 
 }
